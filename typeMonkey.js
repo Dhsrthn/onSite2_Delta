df2b054cd8604a0576b8ffbe09f54137efd9ddcd
Dhsrthn <dhasarathan120904@gmail.com>
2023-08-11 10:23:57 +0530

    make it so timer starts after typing

--- typeMonkey.js
+++ typeMonkey.js
@@ -1,81 +1,77 @@
 typeBackground = document.getElementById('backgroundText')
-const savebutton=document.getElementById('save')
-const startbutton=document.getElementById('start')
-document.body.onkeydown=function(e){
-  if(e.key==' '){
+const savebutton = document.getElementById('save')
+const startbutton = document.getElementById('start')
+const timeH = document.getElementById("time")
+document.body.onkeydown = function (e) {
+  if (e.key == ' ') {
     e.preventDefault()
   }
 }
 
-let savedArray=[]
+let savedArray = []
 let divArray = []
 let curr = 0
 let paragraph
-let object={}
-let wordCount=0
-let correct=0,incorrect=0,uncorrected=0;
-let positions=[]
+let object = {}
+let wordCount = 0
+let correct = 0, incorrect = 0, uncorrected = 0;
+let positions = []
 
 function display(paragraph) {
   console.log(object)
-  typeBackground.innerHTML=''
+  typeBackground.innerHTML = ''
   for (let i = 0; i < paragraph.length; i++) {
     divArray.push([])
     divArray[i] = document.createElement('span')
     divArray[i].textContent = paragraph[i]
     typeBackground.appendChild(divArray[i])
   }
-  const prev=document.getElementById('prevbest')
-  if(object.wpm==0){
-    prev.innerText='Previous: This is the first time you are taking this'
-  }else{
-    prev.innerText=`Previous:  ${(object.wpm).toFixed(2)}`
+  const prev = document.getElementById('prevbest')
+  if (object.wpm == 0) {
+    prev.innerText = 'Previous: This is the first time you are taking this'
+  } else {
+    prev.innerText = `Previous:  ${(object.wpm).toFixed(2)}`
   }
 }
 
-let c=1
+let c = 1
 
 async function runNew() {
-  if(c!=1){
+  if (c != 1) {
     stoptime()
   }
+  timeH.innerHTML = 'Time: 01:00'
   window.removeEventListener('keydown', handleKeyfunction)
   getData()
-  curr=0
-  object={}
-  wordCount=0
-  correct=0
-  incorrect=0
-  uncorrected=0
-  positions=[]
-  startbutton.onclick=startfunction
+  curr = 0
+  object = {}
+  wordCount = 0
+  correct = 0
+  incorrect = 0
+  uncorrected = 0
+  positions = []
+  startbutton.onclick = startfunction
 }
 
-async function getData(){
-  const words=await axios.get('https://random-word-api.vercel.app/api?words=150')
+async function getData() {
+  const words = await axios.get('https://random-word-api.vercel.app/api?words=150')
   console.log(words.data)
-  object.wpm=0
-  object.positions=[]
+  object.wpm = 0
+  object.positions = []
   paragraph = words.data.join(' ')
-  object.paragraph=words.data.join(' ')
+  object.paragraph = words.data.join(' ')
   display(object.paragraph)
 }
 runNew()
 
-
+let start = false
 function startfunction() {
   c++
-  timer(60)
-  startbutton.onclick=null
-  savebutton.onclick=null
+  start = true
   window.addEventListener('keydown', handleKeyfunction)
-  setTimeout(() => {
-    window.removeEventListener('keydown', handleKeyfunction)
-    stoptime()
-    savebutton.onclick=saveState
-    startbutton.onclick=startfunction
-    object.wpm=((((curr+1)/5)-uncorrected))
-  }, 60000)
+  startbutton.onclick = null
+  savebutton.onclick = null
+
 }
 
 
@@ -84,10 +80,22 @@ function startfunction() {
 function handleKeyfunction(e) {
   const validKeys = /^[a-zA-Z\s]$/.test(e.key) || e.key === "Backspace";
   if (validKeys) {
+    if (start) {
+      timer(60)
+      start = false
+      setTimeout(() => {
+        window.removeEventListener('keydown', handleKeyfunction)
+        stoptime()
+        savebutton.onclick = saveState
+        startbutton.onclick = startfunction
+        object.wpm = ((((curr + 1) / 5) - uncorrected))
+      }, 60000)
+
+    }
     if (e.key == "Backspace") {
       if (curr > 0) {
-        console.log(divArray[curr-1].classList.contains('correct'))
-        if(!divArray[curr-1].classList.contains('correct')){
+        console.log(divArray[curr - 1].classList.contains('correct'))
+        if (!divArray[curr - 1].classList.contains('correct')) {
           divArray[curr - 1].classList.remove('correct')
           divArray[curr - 1].classList.remove('incorrect')
           divArray[curr - 1].classList.remove('active')
@@ -116,49 +124,48 @@ function handleKeyfunction(e) {
       incorrect++
       uncorrected++
     }
-    if(object.paragraph[curr]==' '){
+    if (object.paragraph[curr] == ' ') {
       wordCount++
       console.log(wordCount)
     }
   }
-  const accur=document.getElementById("accuracy")
-  accur.innerText=`Accuracy: ${((correct/(correct+incorrect))*100).toFixed(2)}`
+  const accur = document.getElementById("accuracy")
+  accur.innerText = `Accuracy: ${((correct / (correct + incorrect)) * 100).toFixed(2)}`
 }
 
 function timer(t) {
-  const timeH = document.getElementById("time")
-  const wpmH=document.getElementById("currentWPM")
-  
+  const wpmH = document.getElementById("currentWPM")
+
   displayTime(t)
 
   countDown = setInterval(() => {
-      t--
-      positions.push(curr)
-      displayTime(t)
-      if(object.positions.length>0){
-        const ghost=document.getElementsByClassName('ghost')
-        divArray.forEach(element => {
-          element.classList.remove('ghost');
-        });
-        divArray[object.positions[60-t]].classList.add('ghost')
+    t--
+    positions.push(curr)
+    displayTime(t)
+    if (object.positions.length > 0) {
+      const ghost = document.getElementsByClassName('ghost')
+      divArray.forEach(element => {
+        element.classList.remove('ghost');
+      });
+      divArray[object.positions[60 - t]].classList.add('ghost')
 
-      }
+    }
   }, 1000)
 
   function displayTime(t) {
-    let wpm=0
-      if(t!=60){
-        wpm=((((curr+1)/5)-uncorrected)/(60-t))*60
-      }else{
-        wpm=((((curr+1)/5)-uncorrected))
-      }
-      if(wpm<0){
-          wpm=0
-      }
-      let sec = Math.floor(t % 60)
-      let min = Math.floor(t / 60)
-      timeH.innerHTML = `Time: ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`
-      wpmH.innerHTML=`WPM: ${(wpm).toFixed(2)}`
+    let wpm = 0
+    if (t != 60) {
+      wpm = ((((curr + 1) / 5) - uncorrected) / (60 - t)) * 60
+    } else {
+      wpm = ((((curr + 1) / 5) - uncorrected))
+    }
+    if (wpm < 0) {
+      wpm = 0
+    }
+    let sec = Math.floor(t % 60)
+    let min = Math.floor(t / 60)
+    timeH.innerHTML = `Time: ${min < 10 ? "0" : ""}${min}:${sec < 10 ? "0" : ""}${sec}`
+    wpmH.innerHTML = `WPM: ${(wpm).toFixed(2)}`
   }
 }
 
@@ -189,58 +196,59 @@ function closeNav() {
   sidenav.style.width = "0";
 }
 
-function getSavedArray(){
-  let val1=localStorage.getItem('hai')
-  if(val1==null){
-    savedArray=[]
+function getSavedArray() {
+  let val1 = localStorage.getItem('hai')
+  if (val1 == null) {
+    savedArray = []
   }
-  else{
-    savedArray=JSON.parse(val1)
+  else {
+    savedArray = JSON.parse(val1)
   }
 }
 
-function saveState(){
-  object.positions=positions
+function saveState() {
+  object.positions = positions
   savedArray.push(object)
-  localStorage.setItem('hai',JSON.stringify(savedArray))
+  localStorage.setItem('hai', JSON.stringify(savedArray))
   alert('Saved in history')
 }
 
-function displaySaved(){
+function displaySaved() {
   getSavedArray()
-  if(savedArray.length==0){
-    const text=document.createElement('div')
-    text.innerHTML='Your saves appear here'
+  if (savedArray.length == 0) {
+    const text = document.createElement('div')
+    text.innerHTML = 'Your saves appear here'
     text.classList.add('navitems')
     document.getElementById('sidenav').appendChild(text)
   }
-  else{
-    for(let i=0;i<savedArray.length;i++){
-      const saved=document.createElement('div')
+  else {
+    for (let i = 0; i < savedArray.length; i++) {
+      const saved = document.createElement('div')
       saved.classList.add('navitems')
-      saved.style.border='0.1vmin solid black'
-      saved.style.borderRadius='0.5vmin'
-      for(let j=0;j<15;j++){
-        saved.innerHTML+=`${savedArray[0].paragraph[j]}`
+      saved.style.border = '0.1vmin solid black'
+      saved.style.borderRadius = '0.5vmin'
+      for (let j = 0; j < 15; j++) {
+        saved.innerHTML += `${savedArray[0].paragraph[j]}`
       }
       document.getElementById('sidenav').appendChild(saved)
-      saved.addEventListener('click',function(){
-      window.removeEventListener('keydown', handleKeyfunction)
-        object=savedArray[i]
-        divArray=[]
-        curr=0
-        wordCount=0
-        correct=0
-        incorrect=0
-        positions=[]
-        uncorrected=0
-        startbutton.onclick=startfunction
-        if(c!=1){
+      saved.addEventListener('click', function () {
+        window.removeEventListener('keydown', handleKeyfunction)
+        object = savedArray[i]
+        divArray = []
+        curr = 0
+        wordCount = 0
+        correct = 0
+        incorrect = 0
+        positions = []
+        uncorrected = 0
+        timeH.innerHTML = 'Time: 01:00'
+        startbutton.onclick = startfunction
+        if (c != 1) {
 
           stoptime()
         }
         display(object.paragraph)
-        
+
       })
     }
   }
